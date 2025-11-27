b919356fec6f5912b1b0dc37fa4fb9c46655990a
xemenez <EMAIL_XEMENEZ@github.com>
2025-11-26 18:31:29 -0800

    Added a function to add ships of any length

--- src/App.java
+++ src/App.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         // 0 - void, 1 - miss, 20 - ship length 2 alive, 21 - ship length 2 dead, and so on 
         Scanner input = new Scanner(System.in);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
+        
+
         int[][] field = {
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -17,95 +23,44 @@ public class App {
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         };
-        String orientation; 
-        int[] coordsRow = new int[5];
-        int[] coordsColumn = new int[5];
-        boolean is_a_ship = false;
-        int goodRow;
-        int goodColumn;
-        // outer ship entering cycle
-        do{
-            for(int i = 0; i < 10; i++){
+
+        for(int i = 0; i < 10; i++){
                 for(int j = 0; j < 10; j++){
                     if(field[i][j] == 0){
                         System.out.print("?" + " ");
                     }
                 }
                 System.out.println(" ");
-            }
-
-            System.out.println("You are entering a ship with length of 5. Enter if H if you want it to be horizontal of V if vertical");
-            orientation = input.next();
-            while(!orientation.equals("H") && !orientation.equals("V") ){
-                System.out.println("Choose either H or V");
-                orientation = input.next();
-                
-            }
-            System.out.println("Enter the coordinates of the each square of the ship");
-            while(!is_a_ship){
-                for(int i = 0; i < 5; i++){
-                    goodRow = input.nextInt();
-                    while(goodRow < 0 || goodRow >= 10){
-                        System.out.println("Out of bounds; enter a number from 0 to 9");
-                        goodRow = input.nextInt();
-                    }
-                    coordsRow[i] = goodRow;
-                    goodColumn = input.nextInt();
-                    while(goodColumn < 0 || goodColumn >= 10){
-                        System.out.println("Out of bounds; enter a number from 0 to 9");
-                        goodColumn = input.nextInt();
-                    }
-                    coordsColumn[i] = goodColumn;
-                }
-                
-                if(orientation.equals("H")){
-                    is_a_ship = isAShip(coordsRow, coordsColumn,5);
-                }
-
-
-                else{
-                    is_a_ship = isAShip(coordsColumn, coordsRow,5);
-                }
-                if(!is_a_ship){
-                    System.out.println("Not a ship. Enter the coordinates again");
-                }
-            }
-        
-            System.out.println("No space. Enter the ship again");
-
-
-    }while(!canPlace(field, coordsRow, coordsRow, 5));
-            
-    for(int i = 0; i < 5; i++){
-        
-        field[coordsRow[i]][coordsColumn[i]] = 5;
-    }
-    System.out.println("Ship placed succesfully");
-       
-        
-        
-        
-
-
-
-
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            if(field[i][j] == 0){
-                System.out.print("?" + " ");
-            }
-            if(field[i][j] == 5){
-                System.out.print('\u25A0' + " ");
-            }
         }
-        System.out.println(" ");
-    }
+
+
+
+
+        addShip(field, 5);
+        addShip(field, 4);
+        addShip(field, 3);
+        addShip(field, 2);
+        addShip(field, 2);
 
 
 
         
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     public static boolean isAShip(int[] row, int[] column, int length){
@@ -121,7 +76,7 @@ public class App {
         }
         return true;
         
-    
+
 
         
     }
@@ -130,6 +85,9 @@ public class App {
     public static boolean canPlace(int[][] field, int[] row, int[] column, int length){
         for(int i = row[0] - 1; i <= row[length - 1] + 1; i++){
             for(int j = column[0] - 1; j <= column[length - 1] + 1; j++){
+                if((row[0] - 1 < 0) || (row[length - 1] + 1 > 9) || (column[0] - 1 < 0) || (column[length - 1] + 1 > 9)) {
+                    continue;
+                }
                 if(field[i][j] != 0){
                     return false;
                 }
@@ -137,7 +95,95 @@ public class App {
         }
         return true;
     }
-    
-    
 
+    public static void addShip(int[][] field, int length){
+        Scanner input = new Scanner(System.in);
+        String orientation; 
+        int[] coordsRow = new int[5];
+        int[] coordsColumn = new int[5];
+        boolean is_a_ship = false;
+        int goodRow;
+        int goodColumn;
+        // outer ship entering cycle for ship length 5
+        do{
+            is_a_ship = false;
+            
+
+            System.out.println("You are entering a ship with length of " + length + ". Enter if H if you want it to be horizontal of V if vertical");
+            orientation = input.next();
+            while(!orientation.equals("H") && !orientation.equals("V") ){
+                System.out.println("Choose either H or V");
+                orientation = input.next();
+                
+            }
+            System.out.println("Enter the coordinates of the each square of the ship");
+            while(!is_a_ship){
+                for(int i = 0; i < length; i++){
+                    goodRow = input.nextInt();
+                    while(goodRow < 0 || goodRow >= 10){
+                        System.out.println("Out of bounds; enter a number from 0 to 9");
+                        goodRow = input.nextInt();
+                    }
+                    coordsRow[i] = goodRow;
+                    goodColumn = input.nextInt();
+                    while(goodColumn < 0 || goodColumn >= 10){
+                        System.out.println("Out of bounds; enter a number from 0 to 9");
+                        goodColumn = input.nextInt();
+                    }
+                    coordsColumn[i] = goodColumn;
+                }
+                
+                if(orientation.equals("H")){
+                    is_a_ship = isAShip(coordsRow, coordsColumn,length);
+                }
+
+
+                else{
+                    is_a_ship = isAShip(coordsColumn, coordsRow,length);
+                }
+                if(!is_a_ship){
+                    System.out.println("Not a ship. Enter the coordinates again");
+                }
+                
+            }
+            
+            if(!canPlace(field, coordsColumn, coordsRow, length)) {
+                System.out.println("No space. Enter the ship again");
+            }
+        
+            
+
+
+        }while(!canPlace(field, coordsRow, coordsRow, length));
+                
+        for(int i = 0; i < length; i++){
+            
+            field[coordsRow[i]][coordsColumn[i]] = length * 10;
+        }
+        System.out.println("Ship placed succesfully");
+        
+        
+        
+        
+
+
+
+
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(field[i][j] == 0){
+                    System.out.print("?" + " ");
+                }
+                if(field[i][j] > 1){
+                    System.out.print('\u25A0' + " ");
+                }
+            }
+            System.out.println(" ");
+        }
+        
+        
+        System.out.println("");	    
+        System.out.println("");	
+        System.out.println("");	
+    }
 }
